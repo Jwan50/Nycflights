7b3ffb087054d558d352f42eba1e733834371bad
Jwan <https://github.com/Jwan50/Nycflight>
2020-12-09 20:51:45 +0100

    Azure funciton added, test classed added

--- Nycflights/Startup.cs
+++ Nycflights/Startup.cs
@@ -20,6 +20,7 @@ namespace Nycflights
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
